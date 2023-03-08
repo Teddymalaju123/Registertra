81945fd5ae4c092d38393add7df3829a7e9489c7
Teddymalaju123 <127287397+Teddymalaju123@users.noreply.github.com>
2023-03-08 14:17:45 +0700

    addbeernoob

---
+++ app/src/main/java/com/example/registertra/BeeerNoob.kt
@@ -0,0 +1,11 @@
+package com.example.registertra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BeeerNoob : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beeer_noob)
+    }
+}
